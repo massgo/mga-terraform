3408a71d20e6af267b01ffce600a8f055035872d
Andrew Hall <andrew@robinpowered.com>
2016-12-24 20:16:23 -0500

    Fix bug in address module

--- modules/addr/main.tf
+++ modules/addr/main.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "ip" {
 resource "aws_route53_record" "dns"
 {
     zone_id = "${var.zone_id}"
-    name = "${var.name}."
+    name = "${var.name}"
     type = "A"
     ttl = "300"
     records = ["${aws_eip.ip.public_ip}"]
