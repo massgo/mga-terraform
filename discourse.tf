fb386a75d83c86301b6b5bea6d24d22965cfeaf9
Andrew Hall <andrew@robinpowered.com>
2016-12-24 20:08:12 -0500

    Another major restructuring

---
+++ discourse.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "discourse" {
+    ami = "ami-60b6c60a"
+    instance_type = "t2.small"
+    tags = {
+        Name = "Discourse"
+    }
+    monitoring = true
+}
+
+module "discourse_address" {
+  source = "modules/addr"
+  name = "discourse"
+  instance_id = "${aws_instance.discourse.id}"
+  zone_id = "${aws_route53_zone.root.id}"
+}
+
+resource "aws_s3_bucket" "discourse_uploads" {
+    bucket = "massgo-discourse-uploads"
+    versioning { enabled = true }
+    acl = "public_read"
+
+    lifecycle_rule = {
+        id = "purge-tombstone"
+        prefix = "tombstone/"
+        enabled = true
+        expiration = { days = 30 }
+    }
+
+    logging {
+        target_bucket = "${aws_s3_bucket.logs.bucket}"
+        target_prefix = "buckets/discourse-uploads/"
+    }
+}
+
+resource "aws_s3_bucket" "discourse_backups" {
+    bucket = "massgo-discourse-backups"
+    versioning { enabled = true }
+    acl = "private"
+
+    logging {
+        target_bucket = "${aws_s3_bucket.logs.bucket}"
+        target_prefix = "buckets/discourse-backups/"
+    }
+}
---
+++ main.tf
@@ -0,0 +1,78 @@
+provider "aws" { region = "us-east-1" }
+
+resource "aws_s3_bucket" "logs" {
+    bucket = "massgo-logs"
+    versioning { enabled = false }
+    acl = "private"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+    bucket = "massgo-terraform"
+    versioning { enabled = true }
+    acl = "private"
+
+    logging
+    {
+        target_bucket = "${aws_s3_bucket.logs.bucket}"
+        target_prefix = "buckets/terraform/"
+    }
+}
+
+resource "aws_route53_zone" "root" {
+    name = "aws.massgo.org"
+}
+
+resource "aws_security_group" "web-prod" {
+    name = "web_prod"
+    description = "Allow inbound HTTP/S traffic from anywhere"
+
+    ingress
+    {
+        from_port = 0
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress
+    {
+        from_port = 0
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags
+    {
+        Name = "web_prod"
+    }
+}
+
+resource "aws_security_group" "ssh-gbre" {
+    name = "ssh_gbre"
+    description = "Allow SSH traffic from gbre.org"
+
+    ingress
+    {
+        from_port = 0
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["73.234.173.33/32"]
+    }
+
+    tags
+    {
+        Name = "ssh_gbre"
+    }
+}
+
+
+data "terraform_remote_state" "s3" {
+    backend = "s3"
+    config
+    {
+        bucket = "${aws_s3_bucket.tfstate.bucket}"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
