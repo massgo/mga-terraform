2182d637d238191b4fa6f447e975e1882bcdf9fa
Andrew Hall <andrew@robinpowered.com>
2016-12-24 20:26:11 -0500

    Remove old code

--- old/s3/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "logs" {
-    value = ["${aws_s3_bucket.logs.bucket}"]
-}
-
-output "tf-state" {
-    value = ["${aws_s3_bucket.tf-state.bucket}"]
-}
--- old/sg/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "web-prod" {
-    value = ["${aws_security_group.web-prod.id}"]
-}
-
-output "ssh-gbre" {
-    value = ["${aws_security_group.ssh-gbre.id}"]
-}
